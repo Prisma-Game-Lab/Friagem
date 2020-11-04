10b725d095e39376ba9f265202105d7f20b52cbf
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2020-11-04 15:11:09 -0300

    Mecanica de empurrar/puxar caixa

---
+++ Time01/Assets/Scripts/Playerpush.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerpush : MonoBehaviour
+{
+
+    public string BotaoCaixa;
+    public float distance;
+    public LayerMask boxMask;
+    public bool SegurandoCaixahorizontal = false;
+    public bool SegurandoCaixavertical = false;
+
+    GameObject box;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, distance, boxMask);
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, distance, boxMask);
+        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up, distance, boxMask);
+        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down, distance, boxMask);
+
+        //verifica se tem caixa a direita
+        if (hitRight.collider!=null && hitRight.collider.gameObject.tag == "Box" && Input.GetKeyDown(BotaoCaixa))
+        {
+            box = hitRight.collider.gameObject;
+            Debug.Log(hitRight.collider.name);
+            box.transform.SetParent(Player.transform);
+            SegurandoCaixahorizontal = true;
+        }
+        else if(hitRight.collider != null && hitRight.collider.gameObject.tag == "Box" && Input.GetKeyUp(BotaoCaixa))
+        {
+            box.transform.SetParent(null);
+            SegurandoCaixahorizontal = false;
+        }
+
+        //verifica se tem caixa a esquerda
+        if (hitLeft.collider != null && hitLeft.collider.gameObject.tag == "Box" && Input.GetKeyDown(BotaoCaixa))
+        {
+            box = hitLeft.collider.gameObject;
+            Debug.Log(hitLeft.collider.name);
+            box.transform.SetParent(Player.transform);
+            SegurandoCaixahorizontal = true;
+        }
+        else if (hitLeft.collider != null && hitLeft.collider.gameObject.tag == "Box" && Input.GetKeyUp(BotaoCaixa))
+        {
+            box.transform.SetParent(null);
+            SegurandoCaixahorizontal = false;
+        }
+
+        //verifica se tem caixa acima do player
+        if (hitUp.collider != null && hitUp.collider.gameObject.tag == "Box" && Input.GetKeyDown(BotaoCaixa))
+        {
+            box = hitUp.collider.gameObject;
+            Debug.Log(hitUp.collider.name);
+            box.transform.SetParent(Player.transform);
+            SegurandoCaixavertical = true;
+        }
+        else if (hitUp.collider != null && hitUp.collider.gameObject.tag == "Box" && Input.GetKeyUp(BotaoCaixa))
+        {
+            box.transform.SetParent(null);
+            SegurandoCaixavertical = false;
+        }
+
+        //verifica se tem caixa abaixo do player
+        if (hitDown.collider != null && hitDown.collider.gameObject.tag == "Box" && Input.GetKeyDown(BotaoCaixa))
+        {
+            box = hitDown.collider.gameObject;
+            Debug.Log(hitDown.collider.name);
+            box.transform.SetParent(Player.transform);
+            SegurandoCaixavertical = true;
+        }
+        else if (hitDown.collider != null && hitDown.collider.gameObject.tag == "Box" && Input.GetKeyUp(BotaoCaixa))
+        {
+            box.transform.SetParent(null);
+            SegurandoCaixavertical = false;
+        }
+    }
+
+
+
+    //desenha as raios de colisao parao player
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(transform.position, ((Vector2)transform.position + Vector2.right * distance));
+        Gizmos.DrawLine(transform.position, ((Vector2)transform.position + Vector2.left * distance));
+        Gizmos.DrawLine(transform.position, ((Vector2)transform.position + Vector2.up * distance));
+        Gizmos.DrawLine(transform.position, ((Vector2)transform.position + Vector2.down * distance));
+    }
+
+}
