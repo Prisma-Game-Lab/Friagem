cae637c836df220535e7d55d6ac7fbf5639771f9
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2020-11-05 18:07:57 -0300

    Opção de Reset

---
+++ Time01/Assets/Scripts/GameControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public string BotaoRestart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(BotaoRestart)) 
+        {
+            Restart();
+            Debug.Log("Restart");
+        }
+    }
+
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
