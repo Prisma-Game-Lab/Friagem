d1014affb0b72fa362bf960ff502185b6ffbab47
Arthur Ozorio <arthurlozorio@globo.com>
2020-11-13 20:46:06 -0300

    Pequena correção no buraco

--- Time01/Assets/Scripts/Buraco.cs
+++ Time01/Assets/Scripts/Buraco.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Buraco : MonoBehaviour
 {
+    public GameObject player;
+
+    private Playerpush playerPush;
+
+    private void Start()
+    {
+        playerPush = player.GetComponent<Playerpush>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.CompareTag("Box"))
         {
             gameObject.SetActive(false);
             collision.gameObject.SetActive(false);
+            playerPush.SegurandoCaixahorizontal = false;
+            playerPush.SegurandoCaixavertical = false;
         }
     }
 }
