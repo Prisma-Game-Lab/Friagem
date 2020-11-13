0ee6759b7ed050247c6351f6ebc18e36b4ac7160
NagibSuaid <nagibmoura@gmail.com>
2020-11-12 21:36:50 -0300

    Inimigo 'mata' o player

---
+++ Time01/Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TriggerEnter");
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name,LoadSceneMode.Single);
+        }
+    }
+
+}
