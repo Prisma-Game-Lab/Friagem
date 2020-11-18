d50671f9682f11aa3de6af253a3261d753f764c7
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2020-11-18 11:30:45 -0300

    Sistema duplo de Flare
    Agora o flare pode ser limitado a uma quantidade ou pode ser ilimitado em quantidade, mas com um tempo de recarga

--- Time01/Assets/Scripts/Flash.cs
+++ Time01/Assets/Scripts/Flash.cs
@@ -7,26 +7,42 @@ public class Flash : MonoBehaviour
 
     public string BotaoFlash;
     public bool ilumina = false;
+    public bool sistemaDeCargas = false;
     public float tempoIluminado;
+    public float tempoDeRecarga;
     public Vector3 posFlash;
+    public int totalFlares;
+    
 
     public AudioSource flashSound;
     public AudioSource heartbeat;
 
+    private int numFlares;
+    private bool carregado = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        numFlares = totalFlares;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(BotaoFlash))
+        if(Input.GetKeyDown(BotaoFlash) && !sistemaDeCargas && carregado)//numero de flares ilimitados com tempo para recarga
         {
             flashSound.Play();
             heartbeat.Play();
             StartCoroutine(Luz());
+            StartCoroutine(FlareCoolDown());
+        }
+        else if(Input.GetKeyDown(BotaoFlash) && sistemaDeCargas && numFlares > 0)//numero de flares limitados
+        {
+               flashSound.Play();
+               heartbeat.Play();
+               StartCoroutine(Luz());
+               numFlares = numFlares - 1;
+               Debug.Log(numFlares); 
         }
     }
 
@@ -37,5 +53,11 @@ public class Flash : MonoBehaviour
         ilumina = true;
         yield return new WaitForSeconds(tempoIluminado);
         ilumina = false;
+    }
+    private IEnumerator FlareCoolDown()
+    {
+        carregado = false;
+        yield return new WaitForSeconds(tempoDeRecarga);
+        carregado = true;
     }
 }
