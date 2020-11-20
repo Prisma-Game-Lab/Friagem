2ad78d930947ff5bae2b1a982020de9445293a14
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2020-11-19 21:40:17 -0300

    Correção Menu

--- Time01/Assets/Scripts/MainMenu.cs
+++ Time01/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("mark level 5");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
