3d8296f9fa67d2522916b57d30b4d44600c72676
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2020-11-20 12:34:35 -0300

    Mudanças nos menus

--- Time01/Assets/Scripts/MainMenu.cs
+++ Time01/Assets/Scripts/MainMenu.cs
@@ -7,12 +7,17 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void QuitGame()
     {
         Debug.Log("SAIR");
         Application.Quit();
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("Settings Menu");
     }
 }
