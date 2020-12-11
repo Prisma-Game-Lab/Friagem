0fb18ed6abdcafee4497fb0b098b2250edb15466
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2020-12-11 15:58:25 -0300

    FadeOut+FadeIn entre niveis
    Falta restringir o movimento do player no começo do nível e colocar a transição em todos os níveis. Por enquanto está só na sala de testes.

--- Time01/Assets/Scripts/GameControl.cs
+++ Time01/Assets/Scripts/GameControl.cs
@@ -7,13 +7,20 @@ public class GameControl : MonoBehaviour
 {
     public string BotaoRestart;
     public string NextLevel;
-
     public int NextLevelIndex;
+
+    public float firstFlareTime;
+    public Animator transition;
+    public GameObject levelUI;
+    public GameObject Flare;
+    public GameObject Light;
 
     // Start is called before the first frame update
     void Start()
     {
         NextLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        //levelUI.SetActive(true);
+        StartCoroutine(BeginLevel());
     }
 
     // Update is called once per frame
@@ -35,7 +42,8 @@ public class GameControl : MonoBehaviour
                 Debug.Log("Fim do jogo");
             }
 
-            SceneManager.LoadScene(NextLevel, LoadSceneMode.Single);
+            //SceneManager.LoadScene(NextLevel, LoadSceneMode.Single);
+            StartCoroutine(LoadLevel(NextLevel));
 
             if (NextLevelIndex > PlayerPrefs.GetInt("levelReached") && NextLevelIndex != 13)
             {
@@ -48,5 +56,28 @@ public class GameControl : MonoBehaviour
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator LoadLevel(string NextLevel)
+    {
+        transition.SetTrigger("Start");
+        levelUI.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(NextLevel, LoadSceneMode.Single);
+    }
+
+    IEnumerator BeginLevel()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (!Light.activeSelf)
+        {
+            //restringir movimento do player
+            Flare.SetActive(true);
+            yield return new WaitForSeconds(firstFlareTime);
+        }
+        
+        //permitir movimento do player
+        levelUI.SetActive(true);
+        Flare.SetActive(false);
     }
 }
