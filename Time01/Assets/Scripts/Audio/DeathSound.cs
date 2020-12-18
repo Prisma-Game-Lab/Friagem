a910ae273e9953af9c3d90e7114e9a16b829b639
NagibSuaid <nagibmoura@gmail.com>
2020-12-18 11:10:45 -0300

    Som de morte

---
+++ Time01/Assets/Scripts/Audio/DeathSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSound : MonoBehaviour
+{
+    private AudioSource source;
+
+    private void OnEnable() {
+        source = GetComponent<AudioSource>();
+        source.volume = PlayerPrefs.GetFloat("MainPref") * PlayerPrefs.GetFloat("BackgorundPref");
+        source.Play();
+    }
+}
